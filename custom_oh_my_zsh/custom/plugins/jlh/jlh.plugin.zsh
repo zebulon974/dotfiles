2786452aff9cbf274882e8fd7d8df9b80f9f387e
zebulon974 <zebulonfr@hotmail.fr>
2020-04-17 10:17:04 +0200

    modification alias perso

--- custom_oh_my_zsh/custom/plugins/jlh/jlh.plugin.zsh
+++ custom_oh_my_zsh/custom/plugins/jlh/jlh.plugin.zsh
@@ -1,45 +1,33 @@
-## MES ALIAS
+## Mes alias
 #
 alias alais="alias"
-alias la='ls -AlhF --group-directories-first'
+alias la='ls -AFlh --group-directories-first'
+alias rm="rm -I"
 alias lsd='ls -d */'
 alias rmd="rm -rf"
 alias nG='grep -v "^\s*$\|^\s*#.*"'
 alias cls="clear"
 alias hdd="dfc -t ext4"
 alias ping='ping -c3'
+alias srch="yay -Ss"
 alias sa="alias |grep"
 alias kc="killall conky"
-alias edal='vim ~/.oh-my-zsh/custom/jlh.plugin.zsh'
+alias edal='vim ~/.oh-my-zsh/custom/plugins/jlh/jlh.plugin.zsh'
+alias stco='conky -c'
 alias runas="sudo thunar"
 alias gparted="sudo gparted"
 alias dl='youtube-dl'
 alias vi='vim'
 alias view='vim -R'
 alias maj='pacman -Qu | wc -l'
-alias sshsrv='ssh 192.168.0.25'
 alias vimrc='vim ~/.vimrc'
 alias root='sudo su'
 alias xs='cd'
 alias hosts='sh ~/.scripts/hosts.sh'
 alias meteo='curl wttr.in/Biarritz'
 
-setopt correct
-
-##MES FONCTIONS
+# Les alias "YAY"
 #
-#Créer un Dossier et s'y rendre
-function mcd() {
-	mkdir -p "$1" && cd "$1";
-}
-
-#Se deplacer dans un dossier et lister le contenu
-function cdl() {
-	cd $1
-	ls
-}
-
-# Alias pour YAY
 alias yaconf='yay -Pg'
 alias yaupg='yay -Syu'
 alias yasu='yay -Syu --noconfirm'
@@ -55,4 +43,18 @@ alias yalst='yay -Qe'
 alias yaorph='yay -Qtd'
 alias yainsd='yay -S --asdeps'
 alias yamir='yay -Syy'
-alias srch="yay -Ss"
+setopt correct
+
+# Mes Fonctions
+#
+#Créer un Dossier et s'y rendre
+function mcd() {
+	mkdir -p "$1" && cd "$1";
+}
+
+#Se deplacer dans un dossier et lister le contenu
+function cdl() {
+	cd $1
+	ls
+}
+
